e5482da4e81700d7144ac378e55b95274e3a130a
Alexander Messiha <alexander.messiha@outlook.com>
2024-07-15 10:28:25 -0400

    Update _App.jsx

--- src/_App.jsx
+++ src/_App.jsx
@@ -1,1 +1,35 @@
 import React from "react";
+import "./style.css";
+
+function toggleFullscreen() {
+  const elem = document.documentElement;
+  if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    };
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    };
+  };
+};
+
+console.log(document.querySelector('ellipse').getTotalLength());
+
+const App = function() {
+  return (
+    
+  );
+};
